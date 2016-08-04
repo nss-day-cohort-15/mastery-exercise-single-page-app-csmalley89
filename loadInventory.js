8c0e2587ef79a6fcad45696aedef935c9232e634
Chris Smalley <chrism.smalley@gmail.com>
2016-08-04 12:03:18 -0500

    working version of app

--- loadInventory.js
+++ loadInventory.js
@@ -3,21 +3,34 @@ var CarLot = (function (update) {
 
 
   update.activateEvents = function (){
+    $(document).ready(function(){
 
-        $('.card').ready(function(){
-          $('.card').click(function(){
-            $(this).toggleClass('selected');
-            $("#userInput").focus();
-            $('#userInput').on('keyup', function(){
-              $('.card.selected > p').text($('#userInput').val());
-        });
+      // Applies selected class to clicked card and places cursor in textbox
+      $('.card').click(function(){
+        $(this).toggleClass('selected');
+        $("#userInput").focus();
       });
-    });
+
+      // Takes user input and replaces selected card description
+      $('#userInput').on('keyup', function(){
+        $('.card.selected > p').text($('#userInput').val());
+      });
+
+      // Clears the value of input box after user clicks update
+      $('#saveButton').click(function(){
+        $('#userInput').val(' ');
+        $('.card').removeClass('selected');
+      });
+
+      // Prevents the user from using the enter key
+      $('#userInput').keypress(function(e){
+        if(e.which == 13){
+          return false;
+        }
+      });
+    })
 
   }
 
   return update;
 })(CarLot);
-
-
-
